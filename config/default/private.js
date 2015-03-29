10184272ceaa980ea7d41f7bd89a6b42f5838c35
Naoufal Kadhom <naoufalkadhom@gmail.com>
2015-03-29 19:30:05 -0400

    initial commit

---
+++ config/default/private.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AWS_SECRET_KEY: 'x-aws-scret-key',
+  SENDGRID_TOKEN: 'x-send-grid-token'
+};
+
---
+++ config/default/public.js
@@ -0,0 +1,5 @@
+module.exports = {
+  STRIPE_PUBLISHABLE_KEY: 'x-stripe-publishable-key',
+  LANG: process.env.LANG
+};
+
